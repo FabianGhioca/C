424b8c44038c075be3281694a640e93d3f585a55
FabianGhioca <ghiocafabian2004@gmail.com>
2024-12-28 13:42:02 +0100

    corso

---
+++ Lezione1/Corso.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+    
+int main()
+{
+// lezione 06 TIPI DI DATI
+
+//    INT / SHORT / LONG / LONG LONG    NUMERI INTERI                   %d
+//    FLOAT / DOUBLE                    NUMERI CON VIRGOLA              %f
+//    CHAR                              CARATTERI                       %c
+//    BOOL isOnline = true              SE E VERO O FALSO LA FUNZIONE
+//    INT* POINTER                      PUNTATORI
+
+
+
+// lezione 07 CONVERSIONE (CASTING) DEI TIPI
+
+int x = 30;
+float y = x;
+
+printf("ciao ho %f anni", y);
+
+
+
+
+return 0;
+}
